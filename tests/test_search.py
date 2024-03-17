11cad27d3db8721333118d5956d399393f94a4b5
shamayla <shamaylash@hotmail.com>
2024-03-17 20:42:04 +0800

    fixed race condition for Firefox

--- tests/test_search.py
+++ tests/test_search.py
@@ -16,13 +16,13 @@ def test_basic_duckduckgo_search(browser):
     ## When the user searched for "panda" - ACT
     searchpage.search(PHRASE)
 
-    ## Then the search result title contains "panda" - ASSERT
-    assert PHRASE in resultpage.title()
-
-    ## And the search result query contains "panda" - ASSERT
+    ## Then the search result query contains "panda" - ASSERT
     assert PHRASE in resultpage.search_input_value()
     
     ## And the search result links pretain to "panda" - ASSERT
     titles = resultpage.result_link_titles()
     matches  = [t for t in titles if PHRASE.lower() in t.lower()]
     assert len(matches) > 0
+
+    ## And the search result title contains "panda" - ASSERT
+    assert PHRASE in resultpage.title()
