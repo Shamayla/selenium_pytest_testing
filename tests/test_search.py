7ef5b1dfa4c52ae0d6d1b0b4efbcbece0b1822f9
shamayla <shamaylash@hotmail.com>
2024-03-17 21:00:20 +0800

    added parallel test running using pytest-xdist package

--- tests/test_search.py
+++ tests/test_search.py
@@ -3,26 +3,28 @@ These tests covers DuckDuckGo search
 """
 from pages.search import SearchPage
 from pages.result import ResultPage
+import pytest
 
-def test_basic_duckduckgo_search(browser):
+@pytest.mark.parametrize('phrase',['panda', 'python', 'polar bear'])
+def test_basic_duckduckgo_search(browser, phrase):
     
     searchpage = SearchPage(browser)
     resultpage = ResultPage(browser)
 
-    PHRASE = 'panda'
+    #PHRASE = 'panda'
     ## Given the DuckDuckGo home page is displayed - ARRANGE
     searchpage.load()
 
     ## When the user searched for "panda" - ACT
-    searchpage.search(PHRASE)
+    searchpage.search(phrase)
 
     ## Then the search result query contains "panda" - ASSERT
-    assert PHRASE in resultpage.search_input_value()
+    assert phrase in resultpage.search_input_value()
     
     ## And the search result links pretain to "panda" - ASSERT
     titles = resultpage.result_link_titles()
-    matches  = [t for t in titles if PHRASE.lower() in t.lower()]
+    matches  = [t for t in titles if phrase.lower() in t.lower()]
     assert len(matches) > 0
 
     ## And the search result title contains "panda" - ASSERT
-    assert PHRASE in resultpage.title()
+    assert phrase in resultpage.title()
